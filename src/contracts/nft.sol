5fe09bd770b847d631f7f692debc1ca5c9753f7b
Zuhao Chen <zuhao.chen@ucdconnect.ie>
2022-03-20 15:15:27 -0700

    modify market place

--- src/contracts/nft.sol
+++ src/contracts/nft.sol
@@ -1,26 +1,15 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721 {
-
-    // Array that store hash of the pictures
-    string[] public colors; 
-    
-    constructor() ERC721("MyNFT", "MNFT") {
+contract NFT is ERC721URIStorage {
+    uint public tokenCount;
+    constructor() ERC721("DApp NFT", "DAPP"){}
+    function mint(string memory _tokenURI) external returns(uint) {
+        tokenCount ++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return(tokenCount);
     }
-
-
-    function mint(string memory _color, address to) public {
-        console.log("Trying to send tokens to %s", to);
-        colors.push(_color);
-        uint _id = colors.length - 1;
-        console.log("Stored token is %s", _id);
-        _mint(to, _id);
-        address res = ownerOf(_id);
-        console.log("The stored address is %s", res);
-        
-    }
-
 }
